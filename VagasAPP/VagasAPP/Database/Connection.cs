3df2e4aae2ae56b84fc70f279c2b8b216fb4e623
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-31 11:41:59 -0300

    Implementação do banco de dados utilizando DependencyService

---
+++ VagasAPP/VagasAPP/Database/Connection.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace VagasAPP.Database {
+    public class Connection {
+
+        private const string DATABASE_NAME = "data.sqlite";
+
+        private static SQLiteConnection _connection;
+        
+        private static void Open() {
+            var dep = DependencyService.Get<IDatabasePath>();
+            string path = dep.GetPath(DATABASE_NAME);
+            _connection = new SQLiteConnection(path);
+        }
+
+        public static SQLiteConnection Get() {
+            if (_connection == null)
+                Connection.Open();
+            return _connection;
+        }
+
+        public static void Close() {
+            _connection.Close();
+            _connection = null;
+        }
+
+    }
+}
