58454f147b68189c6658f919fe33248645769787
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-31 13:50:28 -0300

    Implementação database para Windows

---
+++ VagasAPP/VagasAPP.UWP/Database/DatabasePathManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VagasAPP.Database;
+using VagasAPP.UWP.Database;
+using Windows.Storage;
+
+[assembly:Xamarin.Forms.Dependency(typeof(DatabasePathManager))]
+namespace VagasAPP.UWP.Database {
+    public class DatabasePathManager : IDatabasePath {
+        public string GetPath(string databaseName) {
+            var usableDirectory = ApplicationData.Current.LocalFolder.Path;
+            return System.IO.Path.Combine(usableDirectory, databaseName);
+        }
+    }
+}
