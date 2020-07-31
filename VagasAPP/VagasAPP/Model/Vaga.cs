8eedc2786ed2d91f24f589c22b9ea9517dc2ad15
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-31 11:01:42 -0300

    Criação do modelo

---
+++ VagasAPP/VagasAPP/Model/Vaga.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VagasAPP.Model {
+    public class Vaga {
+        
+        public string Nome { get; set; }
+        public short Vagas { get; set; }
+        public string Cidade { get; set; }
+        public double Salario { get; set; }
+        public string Descricao { get; set; }
+        public string Tipo { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
