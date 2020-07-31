2590eed8680868bd2bd34248556f515789a0f11b
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-31 11:58:47 -0300

    Implementação da conexão com o banco no Android

---
+++ VagasAPP/VagasAPP.Android/Database/DatabasePathManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using VagasAPP.Database;
+using VagasAPP.Droid.Database;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabasePathManager))]
+namespace VagasAPP.Droid.Database {
+    public class DatabasePathManager : IDatabasePath {
+        public string GetPath(string databaseName) {
+            var dbPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            return System.IO.Path.Combine(dbPath, databaseName);
+        }
+    }
+}
