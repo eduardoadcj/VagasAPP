22d1d3be4315042cb9ba5724c368197cb80173f8
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-31 13:44:32 -0300

    Implementação do banco para iOS

---
+++ VagasAPP/VagasAPP.iOS/Database/DatabasePathManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using VagasAPP.Database;
+using VagasAPP.iOS.Database;
+
+[assembly:Xamarin.Forms.Dependency(typeof(DatabasePathManager))]
+namespace VagasAPP.iOS.Database {
+    public class DatabasePathManager : IDatabasePath {
+
+        public string GetPath(string databaseName) {
+            var useableDir = System.IO.Path.Combine(
+                System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal), "..", "Library");
+            return System.IO.Path.Combine(useableDir, databaseName);
+        }
+
+    }
+}
